f30dcd429b4ff849c79e761847a0b1576d5488bd
TestLeafPages <earth@testleaf.com>
2023-05-10 17:27:07 +0530

    Add files via upload

---
+++ week3/day1/Assignment1.java
@@ -0,0 +1,11 @@
+
+
+  Package   :org.system
+     Class        :Computer
+     Methods   :computerModel()
+
+     Class        :Desktop
+     Methods   :desktopSize()
+
+Description:
+create above 2 class and call all your class methods into the Desktop using single inheritance.
---
+++ week3/day1/Assignment3.java
@@ -0,0 +1,15 @@
+ Package   :org.college
+     Class     :College
+     Methods   :collegeName(),collegeCode(),collegeRank()
+
+     Package   :org.department
+     Class        :Department
+     Methods   :deptName()
+ 
+     Package   :org.student
+     Class        :Student
+     Methods   :studentName(),studentDept(),studentId()
+  
+Description:
+create above 3 class and call all your class methods into the Student using multilevel inheritance.
+
