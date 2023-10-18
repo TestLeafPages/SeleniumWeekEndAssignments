ff189b18db98e8f01aba56dd1e6d6cdcc6a69e75
TestLeafPages <earth@testleaf.com>
2023-10-18 17:32:17 +0530

    Delete week6/day1/ServiceNowTC.java

--- week6/day1/ServiceNowTC.java
+++
@@ -1,6 +0,0 @@
-1. Create BaseClass with preCondition() and postCondition() with @BeforeMethod and @AfterMethod 
-2. Move the common code from all the 5 testcases into preCondition() and postCondition()
-3. Extend all the 5 test cases with BaseClass and execute sequentially and parallely using testng.xml
-
-Implement the above said steps in ServiceNow application  
-for the given 4 testcases of Incident Management in the attached text file
