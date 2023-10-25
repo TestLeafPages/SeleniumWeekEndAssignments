aa0544cf7f73854c60642b87bbd7ed514e38b9db
TestLeafPages <earth@testleaf.com>
2023-10-25 18:08:27 +0530

    Add files via upload

---
+++ week6/day2/PageObjectModel.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class PageObjectModel {
+	  Assignments on Testng and PageObjectModel:
+	
+		1.Try to implementing the attributes are invocationCount and priority.
+		2.Try to Implementing depends on methods..
+		3.Try to Implementing the attributes enabled...
+		4.Try to Implementing the timeout.
+	
+	
+		POM -  ASSIGNMENT
+	
+	Implement the PageObjectModel for all the 5 Testcases in leaftaps.
+	Create, Edit, Delete, Duplicate and MergeLead testcases.
+	Try to run all 5 testcases in Sequence mode.
+
+}
